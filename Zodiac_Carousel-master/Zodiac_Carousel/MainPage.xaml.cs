3a2104d72dd6762f5527404e929b28af89709193
Georgi213 <77331773+Georgi213@users.noreply.github.com>
2022-05-19 12:52:57 +0300

    Add files via upload

---
+++ Zodiac_Carousel-master/Zodiac_Carousel/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Zodiac_Carousel
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                Children.Add(new Zo(i));
+            }
+        }
+    }
+}
---
+++ Zodiac_Carousel-master/Zodiac_Carousel/Zo.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zodiac_Carousel
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Zo : ContentPage
+    {
+        private int IndexOfTextArray = 1;
+        private string[] Arraytext;
+        public Zo(int _index)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            Arraytext = Properties.Resources.ResourceManager.GetString("_" + _index.ToString()).Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            en.Text = Arraytext[0];
+            img.Source = ImageSource.FromResource("Zodiac_Carousel.Resources._" + _index.ToString() + ".jpg");
+        }
+
+
+        private void MoreText(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            var returnString = Arraytext[IndexOfTextArray];
+            if (IndexOfTextArray == Arraytext.Length-1)
+            {
+                IndexOfTextArray = 0;
+            }
+            else
+            {
+                IndexOfTextArray++;
+            }
+            en.Text = returnString;
+        }
+    }
+}
